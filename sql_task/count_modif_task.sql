21efd4407963473e6ca9a912589288733ce4d39b
bbhkls <kimliserp@gmail.com>
2024-10-29 15:58:22 +0300

    create task about count  modif in tables

---
+++ sql_task/count_modif_task.sql
@@ -0,0 +1,14 @@
+-- Задача:
+/*
+ Найти таблицы, в которых было много изменений (Insert, Update, Delete).
+ Вывести в поорядке убывания.
+ */
+
+select table_name, (inserts + updates + deletes) modif
+from (
+	select relname AS table_name, sum(n_tup_ins) AS inserts, sum(n_tup_upd) AS updates, sum(n_tup_del) AS deletes
+	from pg_stat_all_tables pgst join pg_tables pt on pgst.relname = pt.tablename
+	where pt.tableowner = 'serps'
+	group by relname
+)
+order by modif desc
